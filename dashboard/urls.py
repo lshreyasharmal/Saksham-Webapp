646cd2ab469179f34aadb3d8adef89ecdd1644f7
Shreya Sharma <shreya15096@iiitd.ac.in>
2019-04-25 19:13:56 +0530

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,6 @@ from dashboard import views
 
 urlpatterns = [
     url(r'^$', views.get_patients),
-    url(r'^(?P<my_id>\w+)',views.get_patient_info),
     url(r'^(?P<my_id>\w+@\w+.\w+.\w+)',views.get_patient_info),
+    url(r'^(?P<my_id>\w+)',views.get_patient_info),
 ]
